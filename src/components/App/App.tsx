09a812a11cb634b8c2081bff9664f9a31661b5fd
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-03 17:25:16 +0100

    Recorder component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Recorder from "../Recorder";
 
 function App() {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <Recorder />
+    </div>
   );
 }
 
