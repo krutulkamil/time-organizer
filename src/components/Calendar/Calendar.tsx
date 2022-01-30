5db73b661798ea7f643c0b9d7c4870c315f7478e
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-30 21:52:40 +0100

    deleting events

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -4,6 +4,7 @@ import {connect, ConnectedProps} from 'react-redux';
 import {RootState} from "../../redux/store";
 import {selectUserEventsArray, loadUserEvents, UserEvent} from "../../redux/user-events";
 import {addZero} from "../../lib/utils";
+import EventItem from "./EventItem";
 
 const mapState = (state: RootState) => ({
     events: selectUserEventsArray(state)
@@ -82,15 +83,7 @@ const Calendar: FC<Props> = ({events, loadUserEvents}) => {
                     </div>
                     <div className="calendar-events">
                         {events.map(event => {
-                            return (
-                                <div key={event.id} className="calendar-event">
-                                    <div className="calendar-event-info">
-                                        <div className="calendar-event-time">10:00 - 12:00</div>
-                                        <div className="calendar-event-title">{event.title}</div>
-                                    </div>
-                                    <button className="calendar-event-delete-button">&times;</button>
-                                </div>
-                            )
+                            return <EventItem key={`event_${event.id}`} event={event} />
                         })}
                     </div>
                 </div>
